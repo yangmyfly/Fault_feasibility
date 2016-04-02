d47b3c915b9dbfe1ba6f59b5397b57717df8ae4d
wilsoncao <275239608@qq.com>
2016-04-02 10:51:47 -0400

    update

--- CreateLaunchConfiguration/DescribeInstances.sh
+++ CreateLaunchConfiguration/DescribeInstances.sh
@@ -11,7 +11,9 @@ if [ $scenarios = 1 ] ; then
     aws ec2 run-instances --image-id ami-1a2b3c4d --count 1 --instance-type t1.micro --key-name MyKeyPair --security-groups MySecurityGroup
 
 
-    # fault inject (you need to find out the what is the instance-id you have created)
+    # fault inject (you need to find out the what is the instance-id you have created and terminiate the instance)
+
+    aws ec2 terminate-instances --instance-ids i-5203422c
 
     aws ec2 describe-instances --instance-ids i-5203422c
 
